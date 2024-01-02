30975195f8bde8b494d5e7f6ee7367df596cd1ea
Sreenidhi R <91629420+Sreenidhi-1@users.noreply.github.com>
2024-01-02 23:28:32 +0530

    Update Plots.py

--- Pages/Plots.py
+++ Pages/Plots.py
@@ -1,24 +1,23 @@
 import streamlit as st
 import os
 l = os.listdir(r"plots")
+st.write("<h1 style='text-align: center;color:  #FFA500;'>PLOTS</h1><br><br>", unsafe_allow_html=True)
 
 root = "plots"
 page_bg_img = f"""
     <style>
     [data-testid="stAppViewContainer"] > .main {{
-        background: radial-gradient(circle, #FFD700, #FF69B4, #87CEEB, #FFD700, #98FB98, #FFA07A, #ADD8E6, #00FF00, #FF6347, #8A2BE2);
+        
         background-size: 200% 200%;
         animation: radialGradientAnimation 10s infinite linear;
     }}
         [data-testid="stSidebar"] {{
-        background: radial-gradient(circle, #87CEEB, #FF69B4); /* Radial gradient for sidebar */
+    
         background-size: cover;
         background-position: top left;
         background-attachment: local;
     }}
-    [data-testid="stHeader"] {{
-        background: rgba(255, 255, 255, 0.7);
-    }}
+    
     [data-testid="stToolbar"] {{
         right: 2rem;
     }}
